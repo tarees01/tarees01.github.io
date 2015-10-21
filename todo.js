c703362e8b49c1a0371604db93c328002d878866
tarees01 <tarees01@luther.edu>
2015-10-21 14:09:12 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,24 @@
-dsfsdfg
+<script type="text/javascript">
+      NewText = function(){
+        t = document.querySelector("main")
+        ul = document.createElement("ul")
+        li = document.createElement("li")
+        ch = document.createElement("input")
+        ch.type = "checkbox"
+        li1 = document.createTextNode(document.querySelector('#redi').value)
+        li.appendChild(ch)
+        li.appendChild(li1)
+        li.className = document.querySelector('#priority').value
+        ul.appendChild(li)
+        t.appendChild(ul)
+        
+      }
+      boxChecked = function(){
+        if (this.checked){
+          this.parentNode.classList.add("Done");
+      }
+        else{
+          this.parentNode.classList.romove("Done");
+        }
+      }
+    </script>
