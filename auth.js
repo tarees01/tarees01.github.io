27ae4f6196c9aa1009e720a51c0b4e267fc57677
tarees01 <tarees01@luther.edu>
2016-04-08 00:54:54 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 // at https://console.developers.google.com/.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = '58489413372-20hdf7tgi7p50h0djarsef15g9tadil5.apps.googleusercontent.com';
+var OAUTH2_CLIENT_ID = '58489413372-ipbvqd4ro7b863bg67btru22npt0og5g.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube',
   'https://www.googleapis.com/auth/yt-analytics.readonly'
